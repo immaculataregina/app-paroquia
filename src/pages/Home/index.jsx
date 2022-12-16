c042c76c86e081d29c0a740d0891b860dbef34c6
FelipeCyano <felipecyano@gmail.com>
2022-12-16 12:02:44 -0300

    Initial commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import '../../styles/global.css'
+import './styles.css'
+
+export function Home() {
+
+  return (
+    <div className="Home">
+      <h1>
+        Hello, Paróquia!
+      </h1>
+      <p>
+        Parágrafo 1
+      </p>
+      <p>
+        Parágrafo 2
+      </p>
+    </div>
+  )
+}
